6b2b96111c814c46fff78dba98cbc434fa8b7771
htntang <htntang@gmail.com>
2023-05-19 14:16:23 -0600

    started front end and back end scholarship table

--- client/src/Pages/Scholarship.js
+++ client/src/Pages/Scholarship.js
@@ -1,7 +1,59 @@
+import {useState, useEffect} from "react";
+import React from "react";
+import { DataGrid, GridToolbar} from '@mui/x-data-grid';
+
 export default function Scholarship(){
+
+    const [rows, setRows] = useState([])
+
+    const columns = [
+        {field:'scholarship', headerName: 'Scholarship'},
+        {field:'demographic', headerName:'Demographic'},
+        {field:'value', headerName: 'Value'},
+        {field: 'requirements', headerName:'Requirements'},
+        {field:'tags', headerName:'Tags'}
+        ];
+
+    const [fetchedData, setFetchedData] = React.useState(null);
+
+useEffect(() => {
+    populateRows()
+}, [])
+        function populateRows(){
+            fetch('http://localhost:5005/api/scholarships')
+            .then(response => response.json()) 
+      .then(data => { console.log(data
+        );
+        setRows(data)
+        setFetchedData({
+        dataSet: data, 
+        rowLength: 100,
+        })});
+        }
+
     return(
         <>
-        <h1>Scholarships</h1>
+        <div>
+            <DataGrid 
+            sx = {{backgroundColor:'#ffffff', height:600, width: "100%"}}
+            rows={rows}
+            getRowId={(row) => row._id}
+            slots={{toolbar: GridToolbar}} 
+            slotProps={{
+                toolbar: {
+                  showQuickFilter: true,
+                  quickFilterProps: { debounceMs: 500 },
+                },
+              }}
+            
+            columns={[
+                    {field:'scholarship', width:250, headerName: 'Scholarship'},
+                    {field:'demographic', width:200, headerName: 'Demographic'},
+                    {field:'value', width:200, headerName: 'Value'},
+                    {field:'requirements', width:300, headerName: 'Requirements'},
+                    {field:'tags', width:200, headerName: 'Tags'}
+                    ]} />   
+        </div>
         </>
-    )
+    );
 }
--- server/controllers/scholarshipController.js
+++ server/controllers/scholarshipController.js
@@ -1,1 +1,85 @@
-import 
+import Scholarship from "../models/scholarshipModel.js";
+
+// Create a new scholarship.
+const createScholarship = async (req,res) => {
+    try {
+        const newScholarship = new Scholarship(req.body);
+        await newScholarship.save();
+        res.send(newScholarship);
+    } catch(error) {
+        res.status(400).send({ message: error.message });
+    }
+};
+
+// Get all scholarships.
+const getAllScholarships = async (req,res) => {
+    try {
+        const scholarships = await Scholarship.find();
+        res.json(playgrounds);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get a single scholarship by id
+const getScholarshipById = async (req,res) => {
+    try {
+        const scholarship = await Scholarship.findById(req.params.id);
+        if (!scholarship) {
+            return res.status(404).json({ message:'Scholarship not found'});
+        }
+        res.json(scholarship);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Get a single playground by name
+const getScholarshipByName = async (req,res) => {
+    try {
+        const scholarship = await Scholarship.findOne(req.params.name);
+        if (!scholarship) {
+            return res.status(404).json({ message: 'Playground not found' });
+        }
+        res.json(scholarship);
+    } catch(error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+// Update a scholarship by id
+const updateScholarshipById = async (req,res) => {
+    try {
+        const scholarship = await Scholarship.findById(req.params.id);
+        if(!scholarship) {
+            return res.status(404).json({ message: 'Scholarship not found'});
+        }
+        Object.assign(scholarship, req.body);
+        await scholarship.save();
+        res.json(scholarship);
+    } catch (error) {
+        res.status(500).json({ message:error.message });
+    }
+};
+
+// Delete a scholarship by id
+const deleteScholarshipById = async (req,res) => {
+    try {
+        const scholarship = await Scholarship.findByIdAndDelete(req.params.id);
+        if (!scholarship) {
+            return res.status(404).json({ message: 'Scholarship not found' });
+        }
+        res.json({ message: 'Scholarship deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+export {
+    createScholarship,
+    getAllScholarships,
+    getScholarshipById,
+    getScholarshipByName,
+    updateScholarshipById,
+    deleteScholarshipById
+};
