1cb6532ea282c20cb8c58897550ce2e543897342
htntang <htntang@gmail.com>
2023-05-19 10:05:31 -0600

    landing page created

---
+++ client/src/Footer.js
@@ -0,0 +1,13 @@
+export default function Footer(){
+    return(
+        <footer>
+            <a href="">About Us</a>
+            <br />
+            <a href="">Footer1</a>
+            <br />
+            <a href="">Footer2</a>
+            <br />
+            <a href="">Footer3</a>
+        </footer>
+    )
+}
