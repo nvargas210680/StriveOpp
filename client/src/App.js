187d7ba6b8e03d46c4638d4f6beca7ced2c293e4
htntang <htntang@gmail.com>
2023-05-19 10:11:44 -0600

    created more pages and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,26 @@ import Layout from './Layout';
 import HomePage from './Pages/HomePage';
 import LoginPage from './Pages/LoginPage';
 import RegistrationPage from './Pages/RegistrationPage';
+import Blog from './Pages/Blog';
+import Scholarships from './Pages/Scholarships';
+import Contact from './Pages/Contact';
+import About from './Pages/About';
 
 function App() {
   return (
     <>
-
     <NavigationBar />
     <Routes>
       <Route path="/" element={ <Layout />}>
         <Route index element={ <HomePage />} />
         <Route path="/login" element={<LoginPage />} />
-        <Route path="register" element={<RegistrationPage />} />
+        <Route path="/register" element={<RegistrationPage />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/scholarships" element={<Scholarships />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Route>
     </Routes>
-
-    <Footer />
     </>
   );
 }
