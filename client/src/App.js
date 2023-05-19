f8ba749d6008f9454a848f53978812e6bc106362
htntang <htntang@gmail.com>
2023-05-19 10:05:31 -0600

    landing page created

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import NavigationBar from './Pages/navbar.js';
+import Header from './Header';
+import Footer from './Footer';
+import Layout from './Layout';
+import HomePage from './Pages/HomePage';
+import LoginPage from './Pages/LoginPage';
+import RegistrationPage from './Pages/RegistrationPage';
 
 function App() {
   return (
     <>
-    welcome
+
+    <NavigationBar />
+    <Routes>
+      <Route path="/" element={ <Layout />}>
+        <Route index element={ <HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="register" element={<RegistrationPage />} />
+      </Route>
+    </Routes>
+
+    <Footer />
     </>
   );
 }
