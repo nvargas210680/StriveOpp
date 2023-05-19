6b44ea6f29c61edf0b9721d5266abc0239b6dbde
htntang <htntang@gmail.com>
2023-05-19 10:53:26 -0600

    footer work

--- client/src/Header.js
+++ client/src/Header.js
@@ -2,12 +2,13 @@ import { Link } from 'react-router-dom';
 
 export default function Header(){
     return(
-        <header>
-        <a href="/" class="logo"><img src="https://images.squarespace-cdn.com/content/v1/612dabc42132625e4c2932c5/4d60e94c-dfa6-4c2e-a153-35d9ec01319b/fotor_2023-1-3_21_18_12.png?format=1500w" /></a>
+        <header>  
+        <a href="/" class="logo">StriveOpp</a>
         
         <nav>
-            <Link to="/login"><b>Login |</b></Link>
-            <Link to="/register"><b> Register</b></Link>
+            <Link to="/login"><button>Login</button></Link>
+            <Link to="/register"><button>Register</button></Link>
+            <Link to="/book"><button>Book Appointment</button></Link>
         </nav>
         </header>
     )
