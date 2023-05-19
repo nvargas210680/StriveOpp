a8339ee943e354b144a86f94fc17394edd7532d9
htntang <htntang@gmail.com>
2023-05-19 11:22:31 -0600

    created pages

--- client/src/Pages/HomePage.js
+++ client/src/Pages/HomePage.js
@@ -1,7 +1,25 @@
 export default function HomePage(){
     return(
+        <>
         <div class="box">
-            homepage
+            hero page
         </div>
+
+        <div class="box2">
+            here to support your goals
+        </div>
+
+        <div class="box3">
+            featured articles
+        </div>
+
+        <div class="box4">
+            featured media
+        </div>
+
+        <div class="box5">
+            awards
+        </div>
+        </>
     )
 }
