e81aacbf8a034bc9367bb6d6e8afe1765b9b52e7
Renata Vieira <rrenattavs@gmail.com>
2023-06-19 16:08:46 -0600

    Added Filters and database link

--- client/src/pages/ScholarshipDatabase.jsx
+++ client/src/pages/ScholarshipDatabase.jsx
@@ -1,42 +1,126 @@
-import {useState, useEffect} from "react";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { db } from "../config/firebase"
+import { collection, getDocs, query, where } from "firebase/firestore";
 import { DataGrid, GridToolbar} from '@mui/x-data-grid';
 
 export default function Scholarship(){
 
     const [rows, setRows] = useState([])
+    const [scholarships, setScholarships] = useState([]);
+    const [filteredScholarships, setFilteredScholarships] = useState([]);
+    const [tagFilter, setTagFilter] = useState("");
+    const [demographicFilter, setDemographicFilter] = useState("all");
 
     const columns = [
-        {field:'scholarship', headerName: 'Scholarship'},
-        {field:'demographic', headerName:'Demographic'},
-        {field:'value', headerName: 'Value'},
-        {field: 'requirements', headerName:'Requirements'},
-        {field:'tags', headerName:'Tags'}
-        ];
+        { field: "scholarship", headerName: "Scholarship", width: 200 },
+        { field: "deadline", headerName: "Deadline", width: 150 },
+        { field: "value", headerName: "Value", width: 120 },
+        { field: "demographic", headerName: "Demographic", width: 200 },
+        { field: "requirements", headerName: "Requirements", width: 250 },
+        { field: "tag", headerName: "Tag", width: 120 },
+        { field: "url", headerName: "URL", width: 200 },
+      ];
 
-    const [fetchedData, setFetchedData] = React.useState(null);
+    // const [fetchedData, setFetchedData] = React.useState(null);
 
-useEffect(() => {
-    populateRows()
-}, [])
-        function populateRows(){
-            fetch('http://localhost:5005/api/scholarships')
-            .then(response => response.json()) 
-      .then(data => { console.log(data
-        );
-        setRows(data)
-        setFetchedData({
-        dataSet: data, 
-        rowLength: 100,
-        })});
-        }
+// useEffect(() => {
+//     populateRows()
+// }, [])
+    //     function populateRows(){
+    //         fetch('http://localhost:5005/api/scholarships')
+    //         .then(response => response.json()) 
+    //   .then(data => { console.log(data
+    //     );
+    //     setRows(data)
+    //     setFetchedData({
+    //     dataSet: data, 
+    //     rowLength: 100,
+    //     })});
+    //     }
+
+    useEffect(() => {
+        const getScholarships = async () => {
+          const schoolCollectionRef = collection(db, "scholarships2");
+          const querySnapshot = await getDocs(schoolCollectionRef);
+          const scholarshipData = querySnapshot.docs.map((doc) => ({
+            ...doc.data(),
+            id: doc.id,
+          }));
+          setScholarships(scholarshipData);
+          setFilteredScholarships(scholarshipData);
+        };
+    
+        getScholarships();
+      }, []);
+
+      const handleTagFilterChange = (e) => {
+        const filterValue = e.target.value.toLowerCase();
+        setTagFilter(filterValue);
+        applyFilters(filterValue, demographicFilter);
+      };
+    
+      const handleDemographicFilterChange = (e) => {
+        const filterValue = e.target.value.toLowerCase();
+        setDemographicFilter(filterValue);
+        applyFilters(tagFilter, filterValue);
+      };
+    
+      const applyFilters = (tagFilterValue, demographicFilterValue) => {
+        const filteredData = scholarships.filter((scholarship) => {
+          const tagMatch = scholarship.tag.toLowerCase().includes(tagFilterValue);
+          const demographicMatch = scholarship.demographic
+            .toLowerCase()
+            .includes(demographicFilterValue);
+          return tagMatch && demographicMatch;
+        });
+        setFilteredScholarships(filteredData);
+      };
 
     return(
         <>
         <div>
             <h1>Scholarship Database</h1>
-
-            <DataGrid 
+            <div className="App">
+      {/* <DataFilter 
+        selected ={demographicFilter}
+            onChangeFilter ={filterChangeHandler}
+          /> */}
+      <div className="filter">
+        <label htmlFor="tagFilter">Tag Filter:</label>
+         <select
+          id="tagFilter"
+          value={tagFilter}
+          onChange={handleTagFilterChange}
+        >
+          <option value="">All</option>
+          <option value="high school">High School</option>
+          <option value="bachelor">Bachelor</option>
+          <option value="pos-graduate">Pos graduated</option>
+          <option value="master">Master</option>
+          <option value="undergraduate">Undergraduate</option>
+        </select>
+      </div>
+      <div className="filter">
+        <label htmlFor="demographicFilter">Demographic Filter:</label>
+        <select
+          id="demographicFilter"
+          value={demographicFilter}
+          onChange={handleDemographicFilterChange}
+        >
+          <option value="">All</option>
+          <option value="any">Any</option>
+          <option value="youth">Youth</option>
+          <option value="canadian">Canadian</option>
+          <option value="women">Women</option>
+          <option value="international">International</option>
+        </select>
+      </div>
+      <div style={{ height: 400, width: "100%" }}>
+        <DataGrid sx = {{backgroundColor:'#ffffff', height:600, width: "100%"}}
+        rows={filteredScholarships} columns={columns} />
+      </div>
+    </div>
+            {/* <DataGrid 
             sx = {{backgroundColor:'#ffffff', height:600, width: "100%"}}
             rows={rows}
             getRowId={(row) => row._id}
@@ -47,14 +131,18 @@ useEffect(() => {
                   quickFilterProps: { debounceMs: 500 },
                 },
               }}
+
+            /> */}
             
-            columns={[
-                    {field:'scholarship', width:200, headerName: 'Scholarship'},
-                    {field:'demographic', width:200, headerName: 'Demographic'},
-                    {field:'value', width:100, headerName: 'Value'},
-                    {field:'requirements', width:300, headerName: 'Requirements'},
-                    {field:'tags', width:300, headerName: 'Tags'}
-                    ]} />   
+            {/* columns={[
+                { field: "scholarship", headerName: "Scholarship", width: 200 },
+                { field: "deadline", headerName: "Deadline", width: 150 },
+                { field: "value", headerName: "Value", width: 120 },
+                { field: "demographic", headerName: "Demographic", width: 200 },
+                { field: "requirements", headerName: "Requirements", width: 250 },
+                { field: "tag", headerName: "Tag", width: 120 },
+                { field: "url", headerName: "URL", width: 200 },
+              ]}   */}
         </div>
         </>
     );
