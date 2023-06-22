ac8cf1b404baa68891310b0f23b008291ac53d36
htntang <htntang@gmail.com>
2023-06-22 10:13:03 -0600

    made some more pages for the registration

--- client/src/pages/Registration/3_OtherInfo.jsx
+++ client/src/pages/Registration/3_OtherInfo.jsx
@@ -3,6 +3,19 @@ import React from "react";
 
 export default function OtherInfo(){
     return(
-        <div></div>
+        <>
+        other info
+        <main className="registration-container">
+            <form className="registration-container">
+                <div class="registration-image">
+                    <img className="registration-image" src="https://i.imgur.com/psSbmZW.jpg"/>
+                </div>
+
+                <div className="registration-form1">
+                    <h1><i>Almost there! 👋</i></h1>
+                </div>
+            </form>
+        </main>
+        </>
     )
-    }
+}
