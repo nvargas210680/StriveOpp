3d1f49cf2baa1fa3c3aea13252af8a43b84a8bb0
htntang <htntang@gmail.com>
2023-07-19 11:18:00 -0600

    profile template

--- client/src/Profiles/User/Profile/Profile.jsx
+++ client/src/Profiles/User/Profile/Profile.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "./Profile.css";
 
 const Profile = () => {
     return(
         <div className="profile">
-            profile
+            <div className="profile-left">
+                <div className="container-profile">
+                    <h1>Username</h1>
+                </div>
+
+                <div className="container-profile">
+                    <h1>Your Tags</h1>
+                </div>
+            </div>
+
+            <div className="profile-center">
+                <div className="container-profile">
+                    <h1>Your Scholarships</h1>
+                </div>
+            </div>
+
+            <div className="profile-right">
+                <div className="container-profile">
+                    <h1>$ Funding Available To You</h1>
+                </div>
+            </div>
         </div>
     )
 }
