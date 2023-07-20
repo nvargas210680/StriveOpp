6d66cde0502a960183b993da019bc5d9ed9899dc
Nicolas Vargas <nikolas210680@gmail.com>
2023-07-20 13:36:41 -0600

    changes

--- client/src/Pages/dashboard/Dashboard.jsx
+++ client/src/Pages/dashboard/Dashboard.jsx
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import { Button, Card, Alert } from "react-bootstrap";
 import { useAuth } from "../../contexts/AuthProvider";
 import { Link, useNavigate } from "react-router-dom";
+import SuggestedScholarships from "../ScholarshipDatabase/SuggestedScholarships";
+import ScholarshipDatabase from "../ScholarshipDatabase/ScholarshipDatabase";
 
 const Dashboard = () => {
   const [error, setError] = useState("");
@@ -32,20 +34,8 @@ const Dashboard = () => {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className="text-center mb-4">Profile</h2>
-          {error && <Alert variant="danger">{error}</Alert>}
-          <strong>Email: </strong> {currentUser.email || "No email available"}
-          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
-            Update Profile
-          </Link>
-        </Card.Body>
-      </Card>
-      <div className="w-100 text-center mt-2">
-        <Button variant="link" onClick={handleLogout}>
-          Log Out
-        </Button>
+      <div>
+        <SuggestedScholarships />
       </div>
     </>
   );
